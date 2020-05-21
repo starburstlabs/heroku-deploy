ce949fb1a8ff09e741516a7c4ce119a1ebec7e95
Dan Ferranti <dan.ferranti@gmail.com>
2020-05-21 15:33:21 -0400

    Update index.js

--- index.js
+++ index.js
@@ -21,8 +21,7 @@ const deploy = ({ dontuseforce, app_name, branch, usedocker, dockerHerokuProcess
     if (appdir === "") {
       console.log("Setting git config http.postBuffer size");
       execSync(`git config --global http.postBuffer 524288000`);
-      console.log("Fetch from Heroku to determine if we have authenticated");
-      execSync(`git fetch heroku`);
+
       console.log("Push branch to heroku now");
       execSync(`git push heroku ${branch}:refs/heads/master ${force}`);
     } else {
